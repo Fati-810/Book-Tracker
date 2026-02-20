53ebd07a20f7d3ed4ceeb2876ca293de0bdc3023
Fati-810 <49760018+Fati-810@users.noreply.github.com>
2026-02-20 11:45:34 +0400

    Update server.js

--- server.js
+++ server.js
@@ -243,5 +243,5 @@ app.get('/book/:id', async(req, res) => {
 // Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,"0.0.0.0",  () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on ${PORT}`);
 });
